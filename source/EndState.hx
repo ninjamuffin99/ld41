92a240dbc7946bc329d278529dfd8754fc00a48e
Cameron <cameron.taylor.ninja@gmail.com>
2018-04-23 18:41:40 -0700

    FUKKEN DONE

--- source/EndState.hx
+++ source/EndState.hx
@@ -1,7 +1,9 @@
 package;
 
 import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.graphics.frames.FlxAtlasFrames;
 import flixel.text.FlxText;
 
 /**
@@ -31,19 +33,37 @@ class EndState extends FlxState
 	{
 		super.create();
 		
-		var txt:FlxText = new FlxText(20, 0, FlxG.width - 20, "", 25);
+		var bg:FlxSprite = new FlxSprite( -900, -40).loadGraphic(AssetPaths.BG_Gymnasium__png);
+		bg.setGraphicSize(Std.int(bg.width * 1.2));
+		bg.updateHitbox();
+		add(bg);
+		var txt:FlxText = new FlxText(20, FlxG.height - 48, FlxG.width - 20, "", 16);
+		
+		var jeff:FlxSprite = new FlxSprite(360, 180).loadGraphic(AssetPaths.PG_Johny_win__png);
+		add(jeff);
 		
 		if (youWon)
 		{
 			txt.text = "CONGRATS YOU BECAME PROM QUEEN AND BEAT THAT STINKER GARLIC KID\nPRESS R TO RESTART THE GAME!";
+			
+			var sofi:FlxSprite = new FlxSprite(200, 200);
+			var tex = FlxAtlasFrames.fromSpriteSheetPacker(AssetPaths.goblinSheetAll__png, AssetPaths.goblinSheetAll__txt);
+			sofi.frames = tex;
+			sofi.animation.add("dAttack1", [27, 28, 29, 30, 31, 32, 33, 34], 9, true);
+			sofi.animation.play("dAttack1");
+			add(sofi);
 		}
 		else
 		{
 			txt.text = "OH NO GARLIC KID WON AND STOLE THE KING FROM YOU! YOU LOSE\nPRESS R TO RESTART THE GAME!";
+			var garlic:FlxSprite = new FlxSprite(200, 200).loadGraphic(AssetPaths.stinker__png);
+			garlic.angularVelocity = 60;
+			add(garlic);
 		}
 		
-		txt.screenCenter();
+		txt.screenCenter(X);
 		add(txt);
+		
 			
 	}
 	
